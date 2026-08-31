271182e9f639b475b327d2f1568fc110e6072571
wanghequan <15084005113@163.com>
2026-08-31 13:10:52 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Viral Video Recreation
+===
+通用爆款复刻
+
+快速开始
+---
+如何构建、安装、运行
+
+测试
+---
+如何执行自动化测试
+
+如何贡献
+---
+贡献patch流程及质量要求
+
+版本信息
+---
+本项目的各版本信息和变更历史可以在[这里][changelog]查看。
+
+维护者
+---
+### owners
+* fangmuyuan(fangmuyuan@baidu.com)
+
+### committers
+* fangmuyuan(fangmuyuan@baidu.com)
+
+讨论
+---
+百度Hi交流群：群号
+
+
+[changelog]: http://icode.baidu.com/repos/baidu/wk-strategy/viral_video_recreation/blob/master:CHANGELOG.md
---
+++ viral_video_recreation/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+################################################################################
+#
+# Copyright (c) 2026 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+本文件允许模块包以python -m viral_video_recreation方式直接执行。
+
+Authors: fangmuyuan(fangmuyuan@baidu.com)
+Date:    2026/08/31 13:10:52
+"""
+
+import sys
+from viral_video_recreation.cmdline import main
+sys.exit(main())
